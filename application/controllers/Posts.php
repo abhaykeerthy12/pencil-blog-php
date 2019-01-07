cbee87403fc8726e058cbbb85050681aa83c1764
abhaykeerthy12 <abhaykeerthy12@gmail.com>
2019-01-07 23:24:39 +0530

    changed the all posts listing page ui

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -9,9 +9,23 @@ class Posts extends CI_Controller
         // pagination config
         $config['base_url'] = base_url() . 'posts/index/';
         $config['total_rows'] = $this->db->count_all('pencil_db_posts');
-        $config['per_page'] = 5;
+        $config['per_page'] = 6;
         $config['uri_segment'] = 3;
-        $config['attributes'] = array('class' => 'index_pagination');
+        $config['full_tag_open'] 	= '<div class="pagging text-center"><nav><ul class="pagination">';
+        $config['full_tag_close'] 	= '</ul></nav></div>';
+        $config['num_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+        $config['num_tag_close'] 	= '</span></li>';
+        $config['cur_tag_open'] 	= '<li class="page-item active"><span class="page-link">';
+        $config['cur_tag_close'] 	= '<span class="sr-only">(current)</span></span></li>';
+        $config['next_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+        $config['next_tagl_close'] 	= '<span aria-hidden="true">&raquo;</span></span></li>';
+        $config['prev_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+        $config['prev_tagl_close'] 	= '</span></li>';
+        $config['first_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+        $config['first_tagl_close'] = '</span></li>';
+        $config['last_tag_open'] 	= '<li class="page-item"><span class="page-link">';
+        $config['last_tagl_close'] 	= '</span></li>';
+
 
         // initialize pagination
         $this->pagination->initialize($config);
--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,49 +1,60 @@
-<!-- page starts -->
-<!-- looping through the posts array which contains post data from database -->
-
-<h4 class="center-align" id="index_card_title"><?=$title?></h4>
+<section id="all_blog">
+<div class="row" style="margin: auto;">
+	<div class="container col-sm-12 col-md-12 col-lg-2" style="background-color: blue;"><br>
+		<div class="col-sm-12 col-md-12 col-lg-12">
+			
+		</div>
+	</div>
+	<div class="container col-sm-12 col-md-12 col-lg-10"><br>
+		<div><h1><?=$title?></h1><hr></div>  
+			
+<div class="col-sm-12 col-md-12 col-lg-12">
 
 <?php foreach ($posts as $post): ?>
-<br>
 
-<div class="row card z-depth-5 hoverable" id="index_page_img">
-	<div class="col s12 m6 l4">
-		<!-- post image -->
-		<img class="responsive-img" 
-		src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['pencil_db_posts_post_image']; ?>">
+        <div class="card w-100 p-3 shadow" style="width: 18rem;">
+        <div class="row">
+        <div class="col-sm-12 col-md-4 col-lg-4 d-flex justify-content-center">
+        <img src="<?php echo site_url(); ?>assets/images/posts/<?php echo $post['pencil_db_posts_post_image']; ?>" class="card-img-top" style="height: 200px;width: 100%; align-self: center;">
+        </div>
+        <div class="col-sm-12 col-md-8 col-lg-8">
+
+        <div class="card-body">
+           
+            <a href="" style="text-decoration: none;color: black"><span class="card-title h5" style="max-height: 3em;height: 3em;"><?php echo ucfirst($post['pencil_db_posts_title']); ?></span></a>
+            <br>
+
+            <a href="" style="text-decoration: none;color: black"><p class="card-text" style="margin-top: 1em;"><?php echo word_limiter($post['pencil_db_posts_body'], 20); ?></p></a>
+            <span class="text-muted" style="font-size: 12px;text-decoration: none;">
+					<img src="<?php echo base_url(); ?>/assets/images/images/laptop.svg" class="rounded-circle img-fluid" style="height: 50px;width: 50px;">
+					<span>&nbsp;&nbsp;</span>Abhay<span>&nbsp;&nbsp;</span>|
+					<span>&nbsp;&nbsp;</span><i class="far fa-clock"></i><span>&nbsp;&nbsp;</span><?php echo date('M-Y', strtotime(str_replace('-','/', $post['pencil_db_posts_created_date']))); ?><span>&nbsp;&nbsp;</span>|<span>&nbsp;&nbsp;</span><i class="far fa-eye"></i><span>&nbsp;&nbsp;</span>500<span>&nbsp;&nbsp;</span>
+					|<span>&nbsp;&nbsp;</span><?php echo $post['pencil_db_categories_name']; ?><span>&nbsp;&nbsp;</span>
+				</span>
+        </div>
+        </div>
+        </div>
+        </div>
+		<?php endforeach;?>
+
+     </div>
+	 <br>
+		<!-- pagination -->
+		<nav aria-label="Page navigation">
+			<div class="d-flex justify-content-center">
+			<?php echo $this->pagination->create_links(); ?>
+			</div>
+		</nav>
+		<br>
 	</div>
-
-	<!-- other contents -->
-	<div class="card-content" id="index_page_card_content">
-		<div class="card-title">
-			<!-- title -->
-			<span><h4 style="font-weight: bold"><?php echo ucfirst($post['pencil_db_posts_title']); ?></h4></span>
-
-			<!-- created date and category name -->
-			 
-			<small class="chip z-depth-1">Posted on: &nbsp<?php 
-			  $show_date = date('d-m-y', strtotime(str_replace('-','/', $post['pencil_db_posts_created_date'])));
-			  $show_time = date('h:i a', strtotime(str_replace('-','/', $post['pencil_db_posts_created_date'])));
-			  $show_cat = $post['pencil_db_categories_name'] ;
-			  echo $show_date.' at '.$show_time.' in '.$show_cat;
-			 ?> </small>
-		</div>
-
-		<!-- post body (words are limited to 75, just for the asthetics) -->
-		<p class="flow-text"><?php echo word_limiter($post['pencil_db_posts_body'], 25); ?></p>
-
-		<!-- read more button (its a floating button) -->
-		<a class="btn btn-floating btn-large halfway-fab waves-effect waves-light deep-purple z-depth-5"
-			href="<?php echo site_url('/posts/' . $post['pencil_db_posts_slug']) ?>"><i class="fas fa-angle-double-right"></i></a>
-	</div>
+	
 </div>
+</section>
 
-<br>
-<?php endforeach;?>
-<!-- loop ends -->
-<!-- pagination -->
-<div class="index_pagination center">
-	<?php echo $this->pagination->create_links(); ?>
-</div>
 
-<!-- page ends -->
+
+
+
+
+
+
