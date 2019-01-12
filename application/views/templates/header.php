a4a37bf7ef640c8e26b2ddeff6c69cc60fe27600
abhaykeerthy12 <abhaykeerthy12@gmail.com>
2019-01-12 21:27:16 +0530

    starting to theme profile page

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -23,6 +23,15 @@
 				<li class="nav-item">
 				<a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
 				</li>
+				<!-- if user is logged in -->
+				<?php if($this->session->userdata('logged_in')): ?>
+				<li class="nav-item">
+					<a class="nav-link" href="<?php echo base_url(); ?>users/logout">Logout</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="<?php echo base_url(); ?>users/profile">Profile</a>
+				</li>
+				<?php else : ?>
 				<li class="nav-item dropdown">
 				<a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 					Join
@@ -31,6 +40,7 @@
 					<a class="dropdown-item" href="<?php echo base_url(); ?>users/login">Login</a>
 					<a class="dropdown-item" href="<?php echo base_url(); ?>users/register">Signup</a>
 				</div>
+				<?php endif; ?>
 				</li>
 				<li class="nav-item">
 				<a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
--- application/views/users/profile.php
+++ application/views/users/profile.php
@@ -1,79 +1,24 @@
+<section id="profile_section" style="overflow: hidden">
 <br>
-<section class="center">
-			<div class="container">
-				<div class="row card z-depth-5 hoverable">
-					<div class="col s12 m12 l12">
-						<div class="card card-panel z-depth-5 hoverable">
-            <img style="width: 175px;height: 175px;" class="circle responsive-img hoverable" src="<?php echo site_url(); ?>assets/images/profile/<?php echo $this->session->userdata('user_image');?>">
-            
-            <h4><?php echo ucfirst($this->session->userdata('user_name')); ?></h4>
-              <p class="chip"><?php echo $this->session->userdata('user_username'); ?></p>
-              <p><?php echo $this->session->userdata('user_email'); ?></p>
-              <p><?php echo $this->session->userdata('user_bio');?></p>
-              <p>Posts : &nbsp&nbsp<?php echo $posts; ?></p>
-              <a href="<?php echo base_url(); ?>users/posts" class="btn deep-purple">View your latest creations</a>
-              <!-- edit button -->
-              <a class="btn-floating btn-large waves-effect waves-light halfway-fab right red accent-3 z-depth-5" href="<?php echo base_url(); ?>users/edit/<?php echo $this->session->userdata('user_id'); ?>"><i class="fas fa-pencil-alt"></i></a>
-            </div><br>
-            
-            <!-- admin powers :-P -->
-            <?php if($this->session->userdata('is_admin') == 'yes') : ?>
-            <?php if(sizeof($users) > 1) : ?>
-            <br>
-            <div class="col s12 m12 l12">
-                        
-                <table class="white hoverable centered z-depth-5">
-              <thead>
-                  <tr>
-                      <th colspan="3"><h5 class="center-align">Manage Users</h5></th>
-                  </tr>
-                  <tr style="border-bottom: 1px solid #bdbdbd;">
-                    <th><h6 class="center-align">Users</h6></th>
-                    <th><h6 class="center-align">Block</h6></th>
-                    <th><h6 class="center-align">Delete</h6></th>
-                  </tr>
-              </thead>
-              <tbody>
-              <!-- loop through the users array and get each user  -->
-              <?php foreach ($users as $user): ?>
-              <!-- only show non-admin users -->
-              <?php if($user['pencil_db_users_is_admin'] == 'no') : ?>
-              <tr class="hoverable">    
-                  
-                <!-- column with shows user names -->
-                  <td><h6><?php echo ucfirst($user['pencil_db_users_name']); ?></h6></td>
-
-                <!-- block / unblock user btn -->
-              <?php if($user['pencil_db_users_is_active'] == 'yes') : ?>
-                <form action="<?php echo base_url(); ?>users/block/<?php echo $user['pencil_db_users_id'] ?>" method="POST">
-                      <td><button type="submit" class="btn red accent-3 hoverable waves-effect waves-light"><i class="fas fa-user-minus"></i></button></td>
-                </form>
-              <?php else : ?>
-                <form action="<?php echo base_url(); ?>users/unblock/<?php echo $user['pencil_db_users_id'] ?>" method="POST">
-                      <td><button type="submit" class="btn waves-effect hoverable waves-light"><i class="fas fa-user-plus"></i></button></td>
-                </form>
-              <?php endif; ?>
-                <form action="<?php echo base_url(); ?>users/delete/<?php echo $user['pencil_db_users_id'] ?>" method="POST">
-                        <td><button type="submit" class="btn red accent-3 hoverable waves-effect waves-light"><i class="fas fa-trash-alt"></i></button></td>
-                </form>
-              </tr>
-              <?php endif; ?>
-              <?php endforeach;?>
-              <!-- loop ends -->
-              </tbody>
-              </table>
-              <br>
+<div class="row container" style="margin: auto">
+    <div class="card shadow col-md-12 col-lg-6 container"> 
+        <div class="d-flex justify-content-center">      
+            <img style="width: 220px;height: 220px;margin: 20px" class="card-img-top shadow-lg img-fluid rounded-circle" src="<?php echo site_url(); ?>assets/images/profile/<?php echo $this->session->userdata('user_image');?>">
+        </div>
+            <div class="card-body container text-center">
+                <div class="card-text">
+                        <p><?php echo $this->session->userdata('user_username'); ?></p>
+                        <p><?php echo $this->session->userdata('user_email'); ?></p>
+                        <p><?php echo $this->session->userdata('user_bio');?></p>
+                </div>            
             </div>
-            
-            <?php endif;?>
-            <?php endif;?>
-
-					</div>
-          
-				</div>
-        
-			</div>
-      
+            <div class="card-footer" style="border: none;background-color: #fff">
+                <a href="<?php echo base_url(); ?>users/posts" class="btn btn-primary btn-block shadow">View your latest creations</a>
+            </div><br>
+    </div><br>
+    <div class="col-md-12 col-lg-6"></div>
+</div>
+<br>
 </section>
 
 
